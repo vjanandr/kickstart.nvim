828b287c6d55ebd6b9fedb80c805bfd1ae2c08e8
Vijay Rajamani <vijayr@arrcus.com>
2025-09-11 22:01:42 +0530

    context

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -260,4 +260,18 @@ return {
       vim.cmd [[hi OctoEditable guibg=none]]
     end,
   },
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    opts = {
+      enable = true,
+      max_lines = 3,
+      min_window_height = 0,
+      line_numbers = true,
+      multiline_threshold = 20,
+      trim_scope = 'outer',
+      mode = 'cursor',
+      separator = nil,
+      zindex = 20,
+    },
+  },
 }
