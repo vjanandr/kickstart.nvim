48ec56170ace8a1098219100eb10d4189321d955
Vijay Rajamani <vijayr@arrcus.com>
2026-01-13 16:51:46 +0530

    fix configs

--- init.lua
+++ init.lua
@@ -1458,44 +1458,40 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     event = { 'BufReadPost', 'BufNewFile' },
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    opts = {
-      ensure_installed = { 'bash', 'c', 'cpp', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'rust', 'toml' },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        disable = function(_, buf)
-          local name = vim.api.nvim_buf_get_name(buf)
-          local ok, st = pcall((vim.uv or vim.loop).fs_stat, name)
-          if ok and st and st.size and st.size > 200 * 1024 then
-            return true
-          end
-          local lc = vim.api.nvim_buf_line_count(buf)
-          return lc > 5000
-        end,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = {
-        enable = true,
-        disable = function(lang, buf)
-          if lang == 'ruby' then
-            return true
-          end
-          return vim.api.nvim_buf_line_count(buf) > 5000
-        end,
-      },
-    },
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+    -- We use 'config' instead of 'main' or 'opts' to avoid the path error
+    config = function()
+      local configs = require('nvim-treesitter.configs')
+
+      configs.setup({
+        ensure_installed = {
+          'bash', 'c', 'cpp', 'diff', 'html', 'lua', 'luadoc',
+          'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'rust', 'toml'
+        },
+        auto_install = true,
+        highlight = {
+          enable = true,
+          disable = function(_, buf)
+            local name = vim.api.nvim_buf_get_name(buf)
+            local ok, st = pcall((vim.uv or vim.loop).fs_stat, name)
+            if ok and st and st.size and st.size > 200 * 1024 then
+              return true
+            end
+            local lc = vim.api.nvim_buf_line_count(buf)
+            return lc > 5000
+          end,
+          additional_vim_regex_highlighting = { 'ruby' },
+        },
+        indent = {
+          enable = true,
+          disable = function(lang, buf)
+            if lang == 'ruby' then
+              return true
+            end
+            return vim.api.nvim_buf_line_count(buf) > 5000
+          end,
+        },
+      })
+    end,
   },
   {
     'mrcjkb/rustaceanvim',
